653418a20b563837da218bcaf6f70cb5cd3dd27f
Igor Lima Charles <igorlc2004@gmail.com>
2026-07-04 21:54:01 -0300

    Criação dos métodos findByEmail e findByName

--- src/api/controllers/user/route.ts
+++ src/api/controllers/user/route.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { create } from "./create";
 import { findById } from "./find-by-id";
+import { findByEmail } from "./find-by-email";
+import { findByName } from "./find-by-name";
 
 const userRouter = Router();
 
 userRouter.post("/", create);
 userRouter.get("/:id", findById);
+userRouter.get("/email/:email", findByEmail);
+userRouter.get("/name/:name", findByName);
 
 export default userRouter;
